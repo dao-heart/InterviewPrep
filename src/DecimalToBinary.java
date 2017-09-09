46a31e9122a6b961557fc8a28fe9af5e49daf3c1
gaurangmhatre <gaurangmhatre@gmail.com>
2017-09-09 16:32:03 -0700

    Commit

---
+++ src/DecimalToBinary.java
@@ -0,0 +1,27 @@
+public class DecimalToBinary {
+
+    public static void main(String[] args) {
+        System.out.println("Binary :"+convert(10, 2) );
+
+        System.out.println("Octa :"+convert(10, 7) );
+
+        System.out.println("Traya :"+convert(2, 3) );
+
+
+    }
+
+    public static String convert(int number, int converter){
+        String output = "";
+
+        int processNumber = number;
+
+        while (processNumber>1) {
+            output += processNumber % converter;
+            processNumber = processNumber / converter;
+        }
+        output+=processNumber;
+
+
+        return new StringBuffer(output).reverse().toString();
+    }
+}
