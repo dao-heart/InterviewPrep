d1a5cec3798b11d7bc4ed16a9baf33b351bd3a9f
gaurangmhatre <gaurangmhatre@gmail.com>
2017-10-09 21:22:49 -0700

    MargeSort

--- src/MargeSort.java
+++ src/MargeSort.java
@@ -2,13 +2,41 @@ import java.util.ArrayList;
 
 public class MargeSort
 {
-//    public static ArrayList<Integer> margeSort(int [] a, int [] b){
-//        ArrayList<Integer> margedList = new ArrayList<>();
-//
-//    }
+    public static ArrayList<Integer> margeSort(int [] a, int [] b){
+        ArrayList<Integer> margedList = new ArrayList<>();
+
+        int i=0,j=0;
+        while(i<a.length && j<b.length){
+            if(a[i]<=b[j]){
+                margedList.add(a[i]);
+                i++;
+            }
+            else{
+                margedList.add(b[j]);
+                j++;
+            }
+        }
+
+        if(i<a.length){
+            margedList.add(a[i]);
+        }
+        if(j<b.length){
+            margedList.add(b[j]);
+        }
+
+        return margedList;
+    }
 
 
     public static void main(String[] args) {
+        System.out.println("MargeSort the arrays :");
 
+        int [] a = new int []{1,4,7,9,99};
+        int [] b = new int []{2,3,8,11};
+
+        System.out.println("Array 1: "+a[0] + a[1] + a[2]);
+        System.out.println("Array 2: "+b.toString());
+
+        System.out.println("Marge Sorted Array: "+margeSort(a,b).toString());
     }
 }
